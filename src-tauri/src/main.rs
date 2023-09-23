3917f495c8bc79abc6ce3f73b0a2502fe82cf91b
Reynaldi Chernando <reynaldichernando@gmail.com>
2023-09-23 13:31:21 +0700

    Initial commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,15 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::Manager;
+
+fn main() {
+    tauri::Builder::default()
+      .setup(|app| {
+        let window = app.get_window("main").unwrap();
+        let _ = window.eval("window.location.replace('https://www.notion.so/')");
+        Ok(())
+      })
+      .run(tauri::generate_context!())
+      .expect("error running tauri app");
+  }
